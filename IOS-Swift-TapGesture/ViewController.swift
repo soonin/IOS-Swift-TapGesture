7aaab1fcde175a0dea8ee59244e345830750e591
Pooya Hatami <pooya@node-1w7jr9qro06sxpb1yhqloxnmx.ipv6.telus.net>
2018-08-20 02:26:21 -0700

    done

--- IOS-Swift-TapGesture/ViewController.swift
+++ IOS-Swift-TapGesture/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewTap: UIImageView!
+    var tapGesture = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // TAP Gesture
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.myviewTapped(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        viewTap.addGestureRecognizer(tapGesture)
+        viewTap.isUserInteractionEnabled = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func myviewTapped(_ sender: UITapGestureRecognizer) {
+        
+        if self.viewTap.backgroundColor == UIColor.yellow {
+            self.viewTap.backgroundColor = UIColor.green
+        }else{
+            self.viewTap.backgroundColor = UIColor.yellow
+        }
     }
 
 
